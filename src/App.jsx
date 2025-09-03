fecb40361201102528b62b1abec968042c1392dc
lea <leaw2022@gmail.com>
2025-09-03 00:14:23 -0700

    website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-xl font-bold underline text-blue-500">
+      Tailwind is working! 🎉
+    </h1>
+
+  )
+}
