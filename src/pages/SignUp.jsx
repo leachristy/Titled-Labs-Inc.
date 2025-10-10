c04885122101854e83d0def8dacb7512a1e2050b
Liam <liamnguyen224@gmail.com>
2025-10-10 04:40:40 -0700

    - Add Login logic

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useTheme } from "../contexts/ThemeContext";
+import { UserAuth } from "../contexts/AuthContext.jsx";
 import NavBar from "../components/NavBar";
 
 export default function Signup() {
+  const { googleSignIn, user } = UserAuth();
   const navigate = useNavigate();
   const { currentTheme } = useTheme();
   const isEarthy = currentTheme === "earthy";
@@ -60,6 +62,22 @@ export default function Signup() {
       setIsLoading(false);
     }
   };
+
+  // handle google signin with popup/redirect
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // -> link to dashboard when signin sucessfully
+  useEffect(() => {
+    if (user != null) {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
 
   return (
     <>
@@ -382,7 +400,9 @@ export default function Signup() {
                     d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                   />
                 </svg>
-                <span className="ml-2">Google</span>
+                <span className="ml-2" onClick={handleGoogleSignIn}>
+                  Google
+                </span>
               </button>
 
               <button
